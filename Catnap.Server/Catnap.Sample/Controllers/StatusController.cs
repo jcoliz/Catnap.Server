b5bee61a94301356d5716749380e0792e574e636
James Coliz <jcoliz@xbox.com>
2017-03-28 16:36:00 -0700

    Added proper responses to my sample

--- Catnap.Server/Catnap.Sample/Controllers/StatusController.cs
+++ Catnap.Server/Catnap.Sample/Controllers/StatusController.cs
@@ -18,8 +18,31 @@ namespace Catnap.Sample.Controllers
         [Route]
         public HttpResponse Get()
         {
-            Context.Post(_ => App.Log.Add("status"), null);            
-            return new HttpResponse(HttpStatusCode.Ok, "Test#1");
+            Context.Post(_ => App.Log.Add("status"), null);
+
+
+            var htmlBuilder = new StringBuilder();
+            htmlBuilder.AppendLine("<html>");
+            htmlBuilder.AppendLine("<head>");
+            htmlBuilder.AppendLine("<meta http-equiv=\"refresh\" content=\"10\">");
+            htmlBuilder.AppendLine("<meta http-equiv=\"expires\" content=\"-1\">");
+            htmlBuilder.AppendLine("<script src=\"https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js\"></script>");
+            htmlBuilder.AppendLine("<script>");
+            htmlBuilder.AppendLine("$(document).ready(function(){");
+            htmlBuilder.AppendLine("$(\"button#start\").click(function(){ $.post(\"status/start\",{},function(data,status){ alert (data); }); });");
+            htmlBuilder.AppendLine("$(\"button#stop\").click(function(){ $.post(\"status/stop\",{},function(data,status){ alert (data); }); });");
+            htmlBuilder.AppendLine("});");
+            htmlBuilder.AppendLine("</script>");
+            htmlBuilder.AppendLine("</head>");
+            htmlBuilder.AppendLine("<body>");
+            htmlBuilder.AppendLine($"<h1>{DateTime.Now}</h1>");
+            htmlBuilder.AppendLine("<button id=\"start\">Start</button>");
+            htmlBuilder.AppendLine("<button id=\"stop\">Stop</button>");
+            htmlBuilder.AppendLine("</body>");
+            htmlBuilder.AppendLine("</html>");
+            htmlBuilder.AppendLine();
+
+            return new HttpResponse(HttpStatusCode.Ok, htmlBuilder.ToString());
         }
 
         [HttpPost]
@@ -27,7 +50,7 @@ namespace Catnap.Sample.Controllers
         public HttpResponse Start([Body] string postContent)
         {
             Context.Post(_ => App.Log.Add("status/start"), null);
-            return new HttpResponse(HttpStatusCode.Ok, $"Test#4:{postContent}");
+            return new HttpResponse(HttpStatusCode.Ok, $"Starting...");
         }
 
         [HttpPost]
@@ -35,8 +58,7 @@ namespace Catnap.Sample.Controllers
         public HttpResponse Stop([Body] string postContent)
         {
             Context.Post(_ => App.Log.Add("status/stop"), null);
-            return new HttpResponse(HttpStatusCode.Ok, $"Test#4:{postContent}");
+            return new HttpResponse(HttpStatusCode.Ok, $"Stopping...");
         }
     }
-
 }
